9a9232a112f363ebf4b5ad4a10341dc32a7f434e
Nicholas Ly <lynichol@msu.edu>
2022-09-27 23:42:43 -0400

    Problems 1-2

---
+++ problem1/problem1.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Returns the sum of all multiples of three or five up to a threshold.
+ * 
+ * @param threshold The range (exclusive) of allowed numbers to be summed.
+ * @return Sum of all multiples of three or five.
+ */
+int multiples_of_three_or_five(int threshold) {
+    int sum = 0;
+    for (int i=1; i < threshold; i++) {
+        if ((i % 3 == 0) || (i % 5 == 0)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int sum = multiples_of_three_or_five(1000);
+    cout << sum;
+    return 0;
+}
