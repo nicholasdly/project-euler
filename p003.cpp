7d2dcf424f6d33421b2609005cf843175099d1c5
Nicholas Ly <nichdly@gmail.com>
2022-09-28 01:38:59 -0400

    Update p003.cpp

--- p003.cpp
+++ p003.cpp
@@ -8,7 +8,7 @@ using namespace std;
  * Returns the largest prime factor of a specified number.
  * 
  * @param num The number whose largest prime factor is returned.
- * @return The largest prime factor
+ * @return The largest prime factor.
  */
 int largest_prime_factor(long long int num)
 {
